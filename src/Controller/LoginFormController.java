f1c759f319228c56b7bfdcb74f81b06b50d7171f
chrislabours <67902096+chrislabours@users.noreply.github.com>
2021-09-25 01:08:59 -0700

    Added time zone display to loginForm

--- src/Controller/LoginFormController.java
+++ src/Controller/LoginFormController.java
@@ -2,6 +2,7 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,13 +11,20 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
-public class LoginFormController {
+public class LoginFormController implements Initializable {
     public TextField UsernameText;
     public TextField PasswordText;
     public Label ErrorLabel;
     public Label LocationLabel;
-
+    public ZoneId localZone;
+    //DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm");
 
     public void ToLoginButton(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("../view/ScheduleForm.fxml"));
@@ -29,5 +37,11 @@ public class LoginFormController {
 
     public void ToCancelButton(ActionEvent actionEvent) {
         System.exit(0);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        localZone = ZoneId.systemDefault();
+        LocationLabel.setText(String.valueOf(localZone));
     }
 }
