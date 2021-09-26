41f19bc42e892f9c7feffe5f5b2ac0ca7fea58b1
chrislabours <67902096+chrislabours@users.noreply.github.com>
2021-09-26 01:58:22 -0700

    Added log in attempt tracker in LoginForm

--- src/Controller/LoginFormController.java
+++ src/Controller/LoginFormController.java
@@ -13,8 +13,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -25,7 +29,6 @@ import java.util.ResourceBundle;
 public class LoginFormController implements Initializable {
     public TextField UsernameText;
     public TextField PasswordText;
-    public Label ErrorLabel;
     public Label LocationLabel;
     public ZoneId localZone = ZoneId.systemDefault();
 
@@ -37,14 +40,16 @@ public class LoginFormController implements Initializable {
     ResourceBundle RB = ResourceBundle.getBundle("Login", Locale.getDefault());
     //DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm");
 
-    public void ToLoginButton(ActionEvent actionEvent) {
+    public void ToLoginButton(ActionEvent actionEvent) throws IOException {
         try {
             if (DBUsers.checkLogin(UsernameText.getText(), PasswordText.getText())) {
+                loginAttempt("Failed");
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle(RB.getString("error"));
                 alert.setContentText(RB.getString("error"));
                 alert.showAndWait();
             } else {
+                loginAttempt("Successful");
                 Parent root = FXMLLoader.load(getClass().getResource("../view/ScheduleForm.fxml"));
                 Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
                 Scene scene = new Scene(root);
@@ -54,6 +59,7 @@ public class LoginFormController implements Initializable {
             }
         }
         catch (Exception e) {
+            loginAttempt("Failed");
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle(RB.getString("error"));
             alert.setContentText(RB.getString("error"));
@@ -74,8 +80,16 @@ public class LoginFormController implements Initializable {
         Login.setText(RB.getString("login"));
         Cancel.setText(RB.getString("cancel"));
         TimeZone.setText(RB.getString("timezone"));
+    }
 
-        //ErrorLabel.setText(RB.getString("error"));
+    public void loginAttempt(String attempt) throws IOException {
+        String filename = "login_activity.txt";
+        FileWriter fwriter = new FileWriter(filename, true);
+
+        PrintWriter outputFile = new PrintWriter(fwriter);
+
+        outputFile.println("USER: " + UsernameText.getText() + " DATE/TIME: " + LocalDateTime.now() + " LOGIN: " + attempt);
+        outputFile.close();
     }
 
 
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,13 +8,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Main extends Application {
+
+    ResourceBundle RB = ResourceBundle.getBundle("Login", Locale.getDefault());
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../View/LoginForm.fxml"));
-        primaryStage.setTitle("LOGIN");
+        primaryStage.setTitle(RB.getString("login"));
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
@@ -22,7 +25,7 @@ public class Main extends Application {
 
     public static void main(String[] args) {
 
-        Locale.setDefault(new Locale("fr"));
+        //Locale.setDefault(new Locale("fr"));
 
         JDBC.openConnection();
         launch(args);
